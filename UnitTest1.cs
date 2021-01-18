20698abf3dff4afd53258fefc4036476c6154cff
Juan Marcos Trinidad Maya <jmtrinidad07@gmail.com>
2021-01-17 21:31:48 -0600

    Initial Commit until combo controls
    Initial Commit

---
+++ UnitTest1.cs
@@ -0,0 +1,33 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace SeleniumCSharpNetCore
+{
+    public class Tests : DriverHelper
+    {
+
+        [SetUp]
+        public void Setup()
+        {
+            Console.WriteLine("Setup");
+            Driver = new ChromeDriver();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Driver.Navigate().GoToUrl("http://demowf.aspnetawesome.com/");//http://executeautomation.com
+            Driver.FindElement(By.Id("ContentPlaceHolder1_Meal")).SendKeys("Tomato");
+            Driver.FindElement(By.XPath("//input[@name='ctl00$ContentPlaceHolder1$ChildMeal1']/following-sibling::div[text()='Celery']")).Click();
+
+
+            CustomControl.ComboBox("ContentPlaceHolder1_AllMealsCombo","Almond");
+
+
+            Console.WriteLine("Test 1");
+            Assert.Pass();
+        }
+    }
+}
