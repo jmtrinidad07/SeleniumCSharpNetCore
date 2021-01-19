ac8c95436a60caa1c79d0558050386e31b6a3cae
Juan Marcos Trinidad Maya <jmtrinidad07@gmail.com>
2021-01-18 18:33:08 -0600

    Added code till POM

---
+++ Pages/LoginPage.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeleniumCSharpNetCore.Pages
+{
+    class LoginPage : DriverHelper
+    {
+
+
+        IWebElement txtUsername => Driver.FindElement(By.Id("UserName"));
+        IWebElement txtPassword => Driver.FindElement(By.Id("Password"));
+
+        IWebElement logInButton => Driver.FindElement(By.CssSelector("#loginForm > form > div:nth-child(5) > div > input"));
+
+        public void ClickLogin() => logInButton.Click();
+
+        public void EnterUserNameAndPassword(string userName, string password)
+        {
+            txtUsername.SendKeys(userName);
+            txtPassword.SendKeys(password);
+        
+        }
+
+
+    }
+}
--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,6 +1,7 @@
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using SeleniumCSharpNetCore.Pages;
 using System;
 
 namespace SeleniumCSharpNetCore
@@ -25,9 +26,23 @@ namespace SeleniumCSharpNetCore
 
             CustomControl.ComboBox("ContentPlaceHolder1_AllMealsCombo","Almond");
 
-
-            Console.WriteLine("Test 1");
             Assert.Pass();
         }
+
+        [Test]
+        public void LoginTest()
+        {
+            Driver.Navigate().GoToUrl("http://eaapp.somee.com//");
+            HomePage homePage = new HomePage();
+            LoginPage loginPage = new LoginPage();
+
+            homePage.ClickLogin();
+            loginPage.EnterUserNameAndPassword("admin","password");
+            loginPage.ClickLogin();
+            Assert.That(homePage.IsLogOffExist(), Is.True, "Log off button did not displayed");
+
+        }
+
+
     }
 }
